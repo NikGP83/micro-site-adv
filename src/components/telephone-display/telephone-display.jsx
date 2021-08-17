89bf0fd033abea2844a43f5436b007206d4e097e
Nikolay Golovin <nik.golovin83@gmail.com>
2021-08-17 23:19:17 +0600

    вёрстка заголовок, кнопки, чекбокс, лэйбл

--- src/components/telephone-display/telephone-display.jsx
+++ src/components/telephone-display/telephone-display.jsx
@@ -11,7 +11,8 @@ const TelephoneDisplay = () => {
         console.log(e.target.dataset.num)
     }
     return (
-        <div className="mobile_form">
+        <div className="mobile-form">
+            <div className="mobile-form__wrapper">
                     <h1 className="mobile-form__call-to-action">
                         Введите ваш номер
                         мобильного телефона
@@ -19,7 +20,7 @@ const TelephoneDisplay = () => {
                     <form onSubmit={formHandler} action="post" className="mobile-form__display">
                         <input type="tel" id="mobile-tel"/>
                         <label htmlFor="mobile-tel" className="mobile-form__label">и с Вами свяжется наш менеждер для дальнейшей консультации</label>
-                        <ul onClick={getCons} className="mobile-form__numbers">
+                        <ul onClick={getCons} className="mobile-form__numbers-list">
                             <li><button data-num="1" className="numbers-btn">1</button></li>
                             <li><button data-num="2" className="numbers-btn">2</button></li>
                             <li><button data-num="3" className="numbers-btn">3</button></li>
@@ -29,13 +30,16 @@ const TelephoneDisplay = () => {
                             <li><button data-num="7" className="numbers-btn">7</button></li>
                             <li><button data-num="8" className="numbers-btn">8</button></li>
                             <li><button data-num="9" className="numbers-btn">9</button></li>
-                            <li><button data-num="delete" className="numbers-btn">Стереть</button></li>
+                            <li><button data-num="delete" className="numbers-btn clear--btn">Стереть</button></li>
                             <li><button data-num="0" className="numbers-btn">0</button></li>
                         </ul>
-                        <input type="checkbox" id="user-agree"/>
-                        <label htmlFor="user-agree">Согласие на обработкуперсональных данных</label>
+                        <div className="agree-box__wrapper">
+                        <input type="checkbox" id="user-agree" className="visually-hidden agree-input-checkbox"/>
+                        <label htmlFor="user-agree"><p className="input-label">Согласие на обработку персональных данных</p></label>
+                        </div>
                         <button className="mobile-form__send-btn">Подтвердить номер</button>
                     </form>
+                    </div>
                 </div>
     )
 };
