a430eb8c834899cde76f9429b1928a96854bde6c
Nikolay Golovin <nik.golovin83@gmail.com>
2021-08-18 13:19:59 +0600

    WIP вторая часть экрана вёрстка

--- src/components/appointment-form/appointment-form.jsx
+++ src/components/appointment-form/appointment-form.jsx
@@ -4,16 +4,20 @@ import './appointment-form.scss';
 
 const AppointmentForm = () => {
 
-   
+    
     return (
         <main>
             <section className="app_form">
                 <TelephoneDisplay />
-                <div className="app_form__adv_img">
-                    <div className="close-form"></div>
+                <div className="advant">
+                    <button type="button" className="form-close">
+                        <span className="visually-hidden">Закрыть</span>
+                    </button>
                     <div className="qr-code">
+                    <div className="qr_code__img">
+                            <img src="../../img/index_1.png" alt="qr-код"/>
+                        </div>
                         <p className="qr_code__text">Сканируйте QR-код для получения дополнительной информации</p>
-                        <img src="./img/index_1.png" alt="qr-код" />
                     </div>
                 </div>
             </section>
